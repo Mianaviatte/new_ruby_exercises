e141196e32492db369b0eabb4db03f11cfbaff98
Mianaviatte <52130570+Mianaviatte@users.noreply.github.com>
2022-06-29 02:53:19 +0300

    Update guessing_game.rb

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,4 +1,7 @@
+# you can change the question subject and a secret word here:
+what_is_the = "most important for couple"
 secret_word = "love"
+
 guess = ""
 guess_count = 0
 guess_limit = 5
@@ -6,7 +9,7 @@ out_of_guesse = false
 
 while guess != secret_word and !out_of_guesse
     if guess_count < guess_limit
-        puts "Enter your guess what is the most important for couple?"
+        puts "Enter your guess what is the #{what_is_the}?"
         guess = gets.chomp()
         guess_count += 1
     else
