f68f584973e1b1774ae38f3e3c311bf5905be257
Mianaviatte <52130570+Mianaviatte@users.noreply.github.com>
2022-06-28 17:18:18 +0300

    states of America added

---
+++ roses_are_red.rb
@@ -0,0 +1,10 @@
+puts "What's your favorite color? "
+color = gets.chomp()
+puts "Any plural noun, please: "
+plural_noun = gets.chomp()
+puts "What's your favorite celebrity? "
+celebrity = gets.chomp()
+
+puts "\nRoses are #{color}"
+puts "#{plural_noun} are blue"
+puts "I love #{celebrity}"
